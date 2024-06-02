15b908228136e2c9e12df8f02435c85cd34290da
Jabez Villamarzo <jabezvargas.villamarzo@bicol-u.edu.ph>
2024-06-02 21:16:27 +0800

    feat(clustering): Codify generation of dataset rows

--- Python/static/clustering-script.js
+++ Python/static/clustering-script.js
@@ -58,24 +58,91 @@ function generate_table(_rows) {
 
   const Table = children(document.createElement("table"),
     Header,
-    // Rows,
+    generate_row(),
     children(document.createElement('tr'),
-      children(document.createElement('td'),
+      children((() => {
+	const add_row_cell = document.createElement('td');
+	add_row_cell.setAttribute('colspan', 6);
+	add_row_cell.classList.add('add-row-cell');
+
+	return add_row_cell;
+      })(),
 	add_row_button,
       ),
     ),
   );
-  {
-    const temp = Table.querySelector('td');
-    temp.setAttribute('colspan', 6);
-    temp.classList.add('add-row-cell');
-  }
 
   return Table;
 }
 
 function add_row() {
   alert("here");
+}
+
+function generate_row(_idx) {
+  const form_elements = [];
+  {
+    const DateTime = document.createElement('input');
+    DateTime.setAttribute('type', 'datetime-local');
+
+    form_elements.push(DateTime);
+  }
+  {
+    const Longitude = document.createElement('input');
+    Longitude.setAttribute('type', 'number');
+    Longitude.setAttribute('min', -180);
+    Longitude.setAttribute('max', 180);
+
+    Longitude.setAttribute('value', 0);
+    form_elements.push(Longitude);
+  }
+  {
+    const Latitude = document.createElement('input');
+    Latitude.setAttribute('type', 'number');
+    Latitude.setAttribute('min', -90);
+    Latitude.setAttribute('max', 90);
+
+    Latitude.setAttribute('value', 0);
+    form_elements.push(Latitude);
+  }
+  {
+    const Depth = document.createElement('input');
+    Depth.setAttribute('type', 'number');
+    Depth.setAttribute('min', 0);
+    Depth.setAttribute('max', 999);
+
+    Depth.setAttribute('value', 2);
+    form_elements.push(Depth);
+  }
+  {
+    const Magnitude = document.createElement('input');
+    Magnitude.setAttribute('type', 'number');
+    Magnitude.setAttribute('min', 1.0);
+    Magnitude.setAttribute('max', 10.0);
+    Magnitude.setAttribute('step', 0.1);
+
+    Magnitude.setAttribute('value', 5.0);
+    form_elements.push(Magnitude);
+  }
+  {
+    const Location = document.createElement('input');
+    Location.setAttribute('type', 'text');
+    Location.setAttribute('minlength', 5);
+    Location.setAttribute('maxlength', 255);
+    Location.setAttribute('size', 20);
+
+    Location.setAttribute('placeholder', "25km coast of Bohol...");
+    form_elements.push(Location);
+  }
+ 
+  const Row = children(document.createElement('tr'),
+    form_elements.map(input_el => {
+      return children(document.createElement('td'),
+	input_el
+      );
+    }),
+  );
+  return Row;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
